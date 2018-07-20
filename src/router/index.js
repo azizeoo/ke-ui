c11089cd8fdb683636d903316338a2f2c08f2b68
Abdelaziz BOUSSABAT <aboussabat@norsys.fr>
2018-07-20 17:52:16 +0200

    feat:calculate power init form

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import CalculatePower from '@/components/power/CalculatePower'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/helloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'CalculatePower',
+      component: CalculatePower
     }
   ]
 })
